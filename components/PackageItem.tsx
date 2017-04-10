195937e05ac21ebf2ff37e33a3542e00f658fb7f
Ellensi Rey Chandra <ellensi.chandra@autodesk.com>
2017-04-10 17:12:21 +0800

    Implement selection state

--- components/PackageItem.tsx
+++ components/PackageItem.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 
-export interface PackageItemProps { data: any }
-export interface PackageItemState { expanded: boolean }
+export interface SetSelectionFunction {
+    (index: number): void
+}
+
+export interface PackageItemProps {
+    data: any,
+    index: number,
+    selected: boolean,
+    setSelection: SetSelectionFunction
+}
+
+export interface PackageItemState {
+    expanded: boolean
+}
 
 export class PackageItem extends React.Component<PackageItemProps, PackageItemState> {
+
     constructor(props: PackageItemProps) {
         super(props); // Must call base class
         this.state = { expanded: false }; // Assign initial state
         this.toggleExpandState = this.toggleExpandState.bind(this);
+        this.packageItemClicked = this.packageItemClicked.bind(this);
     }
 
     toggleExpandState() {
@@ -15,12 +29,20 @@ export class PackageItem extends React.Component<PackageItemProps, PackageItemSt
         this.setState({ expanded: !currentState }); // Toggle boolean value
     }
 
+    packageItemClicked() {
+        this.props.setSelection(this.props.index);
+    }
+
     render() {
         let pkg = this.props.data;
+
+        let ItemContainerSelectedOrNot = this.props.selected ? "ItemContainerSelected" : "ItemContainer";
+
         let description = pkg.description;
         if (!this.state.expanded && (pkg.description.length > 140)) {
             description = description.substr(0, 140) + "...";
         }
+
         let expanderText = this.state.expanded ? "[collapse]" : "[expand]";
         let expanderRow = pkg.description.length <= 140 ? "" :
             <div className="ExpanderRow">
@@ -28,7 +50,7 @@ export class PackageItem extends React.Component<PackageItemProps, PackageItemSt
             </div>;
 
         return (
-            <div className="ItemContainer">
+            <div className={ ItemContainerSelectedOrNot } onClick={ this.packageItemClicked } tabIndex={this.props.index}>
                 <div className="ItemLeftPanel">
                     <img className="PackageIcon" src="/resources/icons/package.png" />
                 </div>
--- components/PackageList.tsx
+++ components/PackageList.tsx
@@ -3,13 +3,29 @@ import * as React from "react";
 import { PackageItem } from "./PackageItem";
 
 export interface PackageListProps { packages: any }
+export interface PackageListState { selection: number }
 
-export class PackageList extends React.Component<PackageListProps, undefined> {
+export class PackageList extends React.Component<PackageListProps, PackageListState> {
+
+    constructor(props: PackageListProps) {
+        super(props); // Must call base class
+        this.state = { selection: -1 }; // Assign initial state
+        this.setSelection = this.setSelection.bind(this);
+    }
+
+    setSelection(index: number) {
+        this.setState({ selection: index });
+    }
+
     render() {
 
         let index = 0;
         let packageElements: JSX.Element[] = this.props.packages.map((pkg: any) => 
-            <PackageItem data={ pkg } />
+            <PackageItem
+                selected={ index == this.state.selection }
+                index={ index++ }
+                data={ pkg }
+                setSelection={ this.setSelection }/>
         );
 
         return (<div>{ packageElements }</div>);
